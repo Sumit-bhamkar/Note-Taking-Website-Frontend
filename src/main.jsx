1a1f4891d41fb2aed0debcd24f9419cdb65940f0
Sumit Bhamkar <sumitbhamkar@example.com>
2026-02-26 12:20:29 +0530

    feat: integrate React Query for data fetching in main application

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,18 @@ import App from './App.jsx'
 import { NoteProvider } from "./context/NoteContext.jsx"
 import { AuthProvider } from "./context/AuthContext.jsx"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
-      <AuthProvider>
-         <NoteProvider>
-            <App />
-         </NoteProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+         <AuthProvider>
+            <NoteProvider>
+               <App />
+            </NoteProvider>
+         </AuthProvider>
+      </QueryClientProvider>
    </BrowserRouter>
 );
